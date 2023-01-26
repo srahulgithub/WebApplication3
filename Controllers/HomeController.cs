6280ea0c8bce22667ae739bbda4b1912570a5f87
srahulgithub <75783819+srahulgithub@users.noreply.github.com>
2023-01-26 15:36:27 +0530

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
 using System.Diagnostics;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.GridFS;
+using System.Net.Sockets;
+using Microsoft.Extensions.Options;
+using System.Drawing;
 using WebApplication3.Models;
 
 namespace WebApplication3.Controllers
@@ -23,10 +30,48 @@ namespace WebApplication3.Controllers
             return View();
         }
 
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult AddImage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddImage(UploadImage img)
+        {
+            MongoClient Client = new MongoClient("mongodb://localhost:27017");
+            var db = Client.GetDatabase("ImageDB");
+            var doc = new UploadImage();
+            doc.Title = img.Title;
+            doc.Description = img.Description;
+            doc.Image = img.Image;
+            doc.Id = img.Id;
+            IGridFSBucket bucket = new GridFSBucket(db);
+            var options = new GridFSUploadOptions
+            {
+                ChunkSizeBytes = 64512, // 63KB
+                Metadata = new BsonDocument
+    {
+        { "resolution", "1080P" },
+        { "copyrighted", true }
+    }
+            };
+
+            using var stream = await bucket.OpenUploadStreamAsync(img.Title, options);
+            var id = stream.Id;
+            doc.Image.CopyTo(stream);
+            await stream.CloseAsync();
+
+            return View("Index");
+
+        }
+       
+
+
     }
 }
